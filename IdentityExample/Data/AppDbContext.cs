dd1d877898f93c6b51e0d404fac17f5f1193c3c9
Engin YILDIRIM <engin_yildirim@epam.com>
2022-01-31 11:54:41 +0100

    * Lesson 2 (https://www.youtube.com/watch?v=IjbtWPXVJGw)
    * Added asp.net core identity support ( .AddIdentity)
    * User manager and SigInManager is used for login and register operations
    * Added EF Core support wirh Identity Db Context

---
+++ IdentityExample/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityExample.Data
+{
+    // DBcontext stands for Session in database, we need to register it in very start up of service configuration
+    // IdentityDbContext controls the all of the user tables 
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
